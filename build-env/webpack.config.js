d4afd67ba9730b93f0cb91e1e0e770ae8aebf0ef
Sicheng886 <58097617+Sicheng886@users.noreply.github.com>
2020-07-26 15:05:05 +1000

    更新到提取css文件

---
+++ build-env/webpack.config.js
@@ -0,0 +1,29 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "js/built.js",
+    path: resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    // filename 为可选配置，重命名并设置目录
+    new MiniCssExtractPlugin({
+      filename: "css/build.css",
+    }),
+  ],
+  mode: "development",
+};
